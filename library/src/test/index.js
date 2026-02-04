29f79d1927c15750a64edacb5c37ab975a2eb827
Matthew Day <matthew.day@noaa.gov>
2026-02-03 19:32:42 -0600

    Split draw.jsx into components

--- library/src/test/index.js
+++ library/src/test/index.js
@@ -1,5 +1,6 @@
 import createSounding from '../sounding';
-import { StatsTable, Hodograph } from '../draw';
+import { StatsTable } from '../StatsTable';
+import { Hodograph } from '../Hodograph';
 import data from './soundingData.json'; // assert { type: 'json' };
 
 export { createSounding, StatsTable, Hodograph, data };
