e26c028c06e7a81ed3de405e06da6121b93550ad
Matthew Day <matthew.day@noaa.gov>
2026-02-22 21:07:07 -0600

    Fixed case on import

--- library/src/test/index.js
+++ library/src/test/index.js
@@ -2,7 +2,7 @@ import createSounding from '../soundingModel';
 import StatsTable from '../statsTable/StatsTable';
 import Hodograph from '../hodograph/Hodograph';
 import SkewT from '../skewt/SkewT';
-import SoundingContainer from '../soundingContainer';
+import SoundingContainer from '../SoundingContainer';
 import data from './soundingData.json'; // assert { type: 'json' };
 
 export { createSounding, StatsTable, Hodograph, SkewT, SoundingContainer, data };
